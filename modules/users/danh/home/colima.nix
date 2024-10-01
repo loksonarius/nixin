aaf4bd160f590e02527f8b5f3c4c5de80645b3f0
Dan Herrera <danh@shew.io>
2024-10-01 16:20:20 -0400

    actually enable colima configuration

--- modules/users/danh/home/colima.nix
+++ modules/users/danh/home/colima.nix
@@ -1,5 +1,13 @@
-{ config, lib, system, ... }:
+{ config, lib, system, pkgs, ... }:
 let
   enabled = config.nixin.users.danh.enable
     && lib.strings.hasSuffix "darwin" system;
-in { config = lib.mkIf enabled { home.packages = [ pkgs.colima ]; }; }
+in {
+  config = lib.mkIf enabled {
+    home.packages = [ pkgs.colima ];
+    programs.ssh = {
+      enable = true;
+      includes = [ "/Users/danh/.colima/ssh_config" ];
+    };
+  };
+}
