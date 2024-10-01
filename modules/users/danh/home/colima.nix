5749c3e1cf615d321d2e6ec202ef2e5776515db4
Dan Herrera <danh@shew.io>
2024-10-01 17:17:11 -0400

    idk that colima ssh thing is broke anyway

--- modules/users/danh/home/colima.nix
+++ modules/users/danh/home/colima.nix
@@ -2,12 +2,4 @@
 let
   enabled = config.nixin.users.danh.enable
     && lib.strings.hasSuffix "darwin" system;
-in {
-  config = lib.mkIf enabled {
-    home.packages = [ pkgs.colima ];
-    programs.ssh = {
-      enable = true;
-      includes = [ "/Users/danh/.colima/ssh_config" ];
-    };
-  };
-}
+in { config = lib.mkIf enabled { home.packages = [ pkgs.colima ]; }; }
