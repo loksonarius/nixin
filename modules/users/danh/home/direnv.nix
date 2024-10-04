642625e560c7d0a4cdb5630f9dfac83aea43965a
Dan Herrera <danh@shew.io>
2024-10-04 17:59:08 -0400

    fix(users/danh/direnv): increase timeout warning

--- modules/users/danh/home/direnv.nix
+++ modules/users/danh/home/direnv.nix
@@ -4,6 +4,10 @@ in {
   config = lib.mkIf enabled {
     programs.direnv = {
       enable = true;
+      config.global = {
+        disable_stdin = false;
+        warn_timeout = "30s";
+      };
       nix-direnv.enable = true;
     };
   };
