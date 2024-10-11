f9d676be8c6b499d720abe739ec2a75cf90043dd
Dan Herrera <danh@shew.io>
2024-10-11 13:35:37 -0400

    feat(users/danh/colima): add default settings and aliases

--- modules/users/danh/home/colima.nix
+++ modules/users/danh/home/colima.nix
@@ -2,4 +2,25 @@
 let
   enabled = config.nixin.users.danh.enable
     && lib.strings.hasSuffix "darwin" system;
-in { config = lib.mkIf enabled { home.packages = [ pkgs.colima ]; }; }
+in {
+  config = lib.mkIf enabled {
+    home.packages = [ pkgs.colima ];
+    home.file.".colima/_templates/default.yaml".text = ''
+      cpu: 4
+      disk: 60
+      memory: 8
+      runtime: containerd
+      kubernetes:
+        enabled: true
+        version: v1.30.2+k3s1
+        k3sArgs:
+          - --disable=traefik
+      vmType: qemu
+    '';
+
+    home.shellAliases = {
+      docker = "colima nerdctl";
+      nerdctl = "colima nerdctl";
+    };
+  };
+}
--- modules/users/danh/home/misc-pkgs.nix
+++ modules/users/danh/home/misc-pkgs.nix
@@ -3,5 +3,6 @@ let
   enabled = config.nixin.users.danh.enable;
   isDarwin = lib.strings.hasSuffix "darwin" system;
   commonPkgs = [ pkgs.fd pkgs.jq pkgs.openssh pkgs.teleport pkgs.wget ];
-  darwinPkgs = if isDarwin then [ pkgs.gnutar pkgs.unixtools.watch ] else [ ];
+  darwinPkgs =
+    if isDarwin then [ pkgs.gnutar pkgs.openssh pkgs.unixtools.watch ] else [ ];
 in { config = lib.mkIf enabled { home.packages = commonPkgs ++ darwinPkgs; }; }
